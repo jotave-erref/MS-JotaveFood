1c4535bc159efb7e6fe436ebf52d3fd559aba1f8
jotave-erref <jvrferreira96@gmail.com>
2025-06-08 11:42:19 -0300

    Adicionando serviço gateway

---
+++ gateway/src/test/java/com/jotavefood/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotavefood.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
